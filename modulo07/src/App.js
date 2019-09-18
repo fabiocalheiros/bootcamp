903976ce2cc3cd9a284816a7e80bb150db2ff5d6
Fabio <fabio@Mini-de-fabio.home>
2019-09-17 23:33:04 -0300

    modulo 07 api json server

---
+++ modulo07/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import GlobalStyles from './styles/global';
+import Routes from './routes';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <GlobalStyles />
+      <Header />
+      <Routes />
+    </BrowserRouter>
+  );
+}
+
+export default App;
