b1b6f7801a80b1b2ad6d53150fcc3deb38b3afe2
fabiocalheiros <fabio.calheiros@gmail.com>
2019-10-01 14:47:27 -0300

    react-hooks-end

--- modulo08/src/App.js
+++ modulo08/src/App.js
@@ -1,9 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect, useMemo, useCallback } from 'react';
 
 function App() {
+  const [tech, setTech] = useState([]);
+  const [newTech, setNewTech] = useState('');
+
+  // callback é utilizado pra performance, ele não recria a funcão toda vez que um item do estado é alterado
+  const handleAdd = useCallback(() => {
+    setTech([...tech, newTech]);
+    setNewTech('');
+  }, [newTech, tech]);
+
+  useEffect(() => {
+    const storageTech = localStorage.getItem('tech');
+    if (storageTech) {
+      setTech(JSON.parse(storageTech));
+    }
+
+    //Hooks Componentwillmount
+    //return () => {};
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('tech', JSON.stringify(tech));
+  }, [tech]);
+
+  const techSize = useMemo(() => tech.length, [tech]);
+
   return (
     <>
-      <h1>Hello World</h1>
+      <ul>
+        {tech.map(t => (
+          <li key={t}>{t}</li>
+        ))}
+      </ul>
+      <strong>Você tem {techSize} tecnologias</strong>
+      <br />
+      <input
+        type="text"
+        placeholder="tecnologia"
+        onChange={e => setNewTech(e.target.value)}
+        value={newTech}
+      />
+      <button onClick={handleAdd}>Adicionar</button>
     </>
   );
 }
